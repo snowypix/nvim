0376bea72190251cbcf0d95d30f42e5f515e9eb6
Yassine Zouin <opmail999@gmail.com>
2024-03-27 22:10:48 +0000

    x

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -13,6 +13,24 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   
   {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
+  {
     "ray-x/go.nvim",
     dependencies = {  -- optional packages
       "ray-x/guihua.lua",
